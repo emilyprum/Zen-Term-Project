cfc0ec5b133b04a4eeea6fdfade464768cff7a5f
Emily Prum <prum0006@algonquinlive.com>
2019-03-06 16:13:56 -0500

    tree textured

---
+++ public/js/waterPR.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', init, false);
+
+function init() {
+
+	createSea();
+
+	loop();
+}
+Sea = function()
+{
+
+
+var geom ="/assets/PeaceRoom Models/gardenRiver.obj"
+
+
+}
