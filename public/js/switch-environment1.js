3a8c8022cebd145939573dd223608969bd49f209
emilyprum <emilyprum@cmail.carleton.ca>
2019-03-21 00:53:39 -0400

    transport working on mountain room but had to remove snow :(

--- public/js/switch-environment1.js
+++ public/js/switch-environment1.js
@@ -1,24 +1,3 @@
-AFRAME.registerComponent('switch-environment', {
-    schema: {},
-    init : function() {
-        const Context_AF = this;
-
-
-        Context_AF.el.addEventListener('mousedown', function(event) {
-            console.log("mousedown");
-            Context_AF.switchEnv();
-
-
-        });
-    },
-    switchEnv : function() {
-        const Context_AF = this;
-        location.assign("angerRoom.html"); 
-        
-        let scene = document.querySelector('#PeaceRoom');
-    }
-});
-
 AFRAME.registerComponent('switch-environment1', {
     schema: {},
     init : function() {
@@ -39,5 +18,6 @@ AFRAME.registerComponent('switch-environment1', {
         let scene = document.querySelector('#AngerRoom');
     }
 });
+
 
 
