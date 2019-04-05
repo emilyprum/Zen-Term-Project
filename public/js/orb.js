018deacad3ae7af0ffb64836a6fd2f3c49c4a9ee
Emily Prum <prum0006@algonquinlive.com>
2019-04-05 10:54:21 -0400

    menu pop up

--- public/js/orb.js
+++ public/js/orb.js
@@ -22,13 +22,45 @@ AFRAME.registerComponent('menu-orb', {
     
         let ExitMenu = document.createElement('a-entity');
         ExitMenu.setAttribute('obj-model', {obj:'/assets/OrbMenu/exit_OBJ.obj'});
-        ExitMenu.setAttribute('material', {src:'/assets/Textures/OrbMenu/ExitTXT.png'}, {transparent: true} );
-        ExitMenu.setAttribute('position', {x:-0.242, y:3.6, z:-18.5});
+        ExitMenu.setAttribute('material', {src:'/assets/Textures/OrbMenu/ExitTXT.png'} );
+        ExitMenu.setAttribute('position', {x:-0.05, y:3.7, z:-18.5});
         ExitMenu.setAttribute('rotation', {x:0, y:0, z:0});
         ExitMenu.setAttribute('scale', {x:1, y:1, z:1});
         
+      
+        
+
+
+
+        let BackToMenu = document.createElement('a-entity');
+        BackToMenu.setAttribute('obj-model', {obj:'/assets/OrbMenu/backToMenu_OBJ.obj'});
+        BackToMenu.setAttribute('material', {src:'/assets/Textures/OrbMenu/BackToMenuRoomTXT.png'} );
+        BackToMenu.setAttribute('position', {x:-0.205, y:3.7, z:-18.5});
+        BackToMenu.setAttribute('rotation', {x:0, iy:0, z:0});
+        BackToMenu.setAttribute('scale', {x:1, y:1, z:1});
+
+        let DuoTalkMenu = document.createElement('a-entity');
+        DuoTalkMenu.setAttribute('obj-model', {obj:'/assets/OrbMenu/duoTalk_OBJ.obj'});
+        DuoTalkMenu.setAttribute('material', {src:'/assets/Textures/OrbMenu/duoTalkTXT.png'} );
+        DuoTalkMenu.setAttribute('position', {x:-0.05, y:5, z:-18.5});
+        DuoTalkMenu.setAttribute('rotation', {x:0, y:0, z:0});
+        DuoTalkMenu.setAttribute('scale', {x:1, y:1, z:1});
+        
+
+        let meditateMenu = document.createElement('a-entity');
+        meditateMenu.setAttribute('obj-model', {obj:'/assets/OrbMenu/meditate_OBJ.obj'});
+        meditateMenu.setAttribute('material', {src:'/assets/Textures/OrbMenu/meditateTXT.png'} );
+        meditateMenu.setAttribute('position', {x:-0.205, y:5, z:-18.5});
+        meditateMenu.setAttribute('rotation', {x:0, y:0, z:0});
+        meditateMenu.setAttribute('scale', {x:1, y:1, z:1});
+
+
+
         let scene = document.querySelector('a-scene');
         scene.appendChild(ExitMenu);
+        scene.appendChild(BackToMenu);
+        scene.appendChild(DuoTalkMenu);
+        scene.appendChild(meditateMenu);
 
 
 
