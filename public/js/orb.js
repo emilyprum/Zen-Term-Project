5ee3b64d0f0d5aa762f539a8335cf5a01b8a319d
Emily Prum <prum0006@algonquinlive.com>
2019-04-10 12:10:12 -0400

    final

--- public/js/orb.js
+++ public/js/orb.js
@@ -338,7 +338,7 @@ AFRAME.registerComponent('solace-orb', {
 
         let meditateMenu = document.createElement('a-entity');
         
-        meditateMenu.setAttribute('sound', {src:'/assets/Sounds/SolaceRoom/AnxietyRoomScript.mp3', on:'click', volume:15} );
+        meditateMenu.setAttribute('sound', {src:'/assets/Sounds/SolaceRoom/AnxietyRoomScript.mp3', on:'click', volume:10} );
 
         meditateMenu.setAttribute('obj-model', {obj:'/assets/OrbMenu/meditate_OBJ.obj'});
         meditateMenu.setAttribute('material', {src:'/assets/Textures/OrbMenu/meditateTXT.png'} );
