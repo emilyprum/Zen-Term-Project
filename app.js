95c2f6d41f7134586db3d322322d32e37005401c
Emily Prum <prum0006@algonquinlive.com>
2019-04-10 00:50:18 -0400

    multi device actually works hell ya

--- app.js
+++ app.js
@@ -2,20 +2,57 @@ const express   = require('express');
 const app       = express();
 const http      = require('http');
 const server    = http.createServer(app);
+const socketIO  = require('socket.io')(server); //hello I am new
 
-//const vars
-const LISTEN_PORT = 8080;
+const LISTEN_PORT = 8080; //make sure greater than 3000. Some ports are reserved/blocked by firewall ...
 
-//middleware
-app.use(express.static(__dirname + '/public'));
+app.use((express.static(__dirname + '/public'))); //set root dir to the public folder
 
-//set routes
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + 'public/index.html');
+//routes
+app.get('/color', function(req,res) {
+    res.sendFile(__dirname + '/public/color.html');
 });
 
-//!!TRY: create another route to point to another web page
+app.get('/controller', function(req,res) {
+    res.sendFile(__dirname + '/public/controller.html');
+});
 
-//start server
+//websocket stuff
+socketIO.on('connection', function(socket) {
+    console.log(socket.id + ' has connected!');
+
+    socket.on('disconnect', function(data) {
+        console.log(socket.id + ' has disconnected');
+    });
+
+    //custom events
+    //socket = one client
+    //socketIO.sockets = all clients
+
+    // these are the functions that get passed.. this passes in the color
+    socket.on('Hi', function(data) {  
+        console.log('hi');
+
+
+        var xpos = Math.random() * (10 - 0) + 0;
+        var ypos = Math.random() * (10 - (-5)) + (-5);
+        var zpos = Math.random() * (40 - (-20)) + (-20);
+        socketIO.sockets.emit('HiUser', {xpos, ypos, zpos});
+        
+    });
+
+
+    socket.on('HowDay', function(data) {  
+        console.log('working');
+        var message = "How was your day?"
+        
+        socketIO.sockets.emit('chat message', {message});
+        
+    });
+
+   
+});
+
+//finally, start server
 server.listen(LISTEN_PORT);
-console.log('Listening on port: ' + LISTEN_PORT );
+console.log('listening to port: ' + LISTEN_PORT);
